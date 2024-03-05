655790e9488e92d569918cfa02c5e1ae5e5a4b92
cizal <cizal@esignature.com.np>
2024-03-06 00:08:55 +0545

    Added navigation feature to the Navbar

--- web/src/pages/rangescreen.js
+++ web/src/pages/rangescreen.js
@@ -1,6 +1,7 @@
-import RangeScreen from "./components/RangeScreen";
+import RangeScreen from "../app/components/RangeScreen";
+import "../app/globals.css";
 
-export default function Home() {
+export default function RangeScreenPage() {
   return (
     <main className="flex flex-col min-h-screen justify-center items-center ">
       <RangeScreen />
---
+++ web/src/pages/specsscreen.js
@@ -0,0 +1,10 @@
+import SpecsScreen from "../app/components/SpecsScreen";
+import "../app/globals.css";
+
+export default function SpecsScreenPage() {
+  return (
+    <main className="flex flex-col min-h-screen justify-center items-center ">
+      <SpecsScreen />
+    </main>
+  );
+}
