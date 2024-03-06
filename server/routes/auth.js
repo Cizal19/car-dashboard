3dc8b89127db4d7581cb1503905fc19edad9318b
cizal <cizal@esignature.com.np>
2024-03-06 20:56:37 +0545

    changes token expiration time

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 router.post("/authenticate", (req, res) => {
   const user = { id: 1, username: "user" }; // Mock user
   const token = jwt.sign(user, process.env.JWT_SECRET, {
-    expiresIn: "1h",
+    expiresIn: "4h",
   });
 
   res.send({ token });
